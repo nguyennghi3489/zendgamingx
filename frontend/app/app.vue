5852db9a78a78b1d5bb8ba1cd5e57ec996fb4512
Nghi Nguyen <nguyennghi3489@gmail.com>
2025-11-12 13:48:13 +0700

    feat: add core nuxt modules

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -2,5 +2,6 @@
   <div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
+    <h1 class="p-10">TEsting</h1>
   </div>
 </template>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/ui']
-})
+  modules: [
+    "@nuxtjs/google-fonts",
+    "@nuxt/icon",
+    "@nuxt/ui",
+    "@pinia/nuxt",
+    "dayjs-nuxt",
+  ],
+  googleFonts: {
+    families: {
+      Inter: true,
+    },
+  },
+  css: ["~/assets/css/main.css"],
+});
